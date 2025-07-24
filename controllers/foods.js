93832336933e59248874e7d1048f305e8f9210ea
pericarney00 <pericarney@gmail.com>
2025-07-24 18:29:21 -0500

    I created and completed the functionality for the new.ejs page.

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,4 +5,51 @@ const User = require("../models/user.js");
 
 // router logic will go here - will be built later on in the lab
 
+//GET landing page
+router.get("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id)
+
+    res.render("foods/index.ejs", {
+      foods: currentUser.foods
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.redirect("/")
+  }
+
+})
+
+
+// GET new food
+router.get("/new", async (req, res) => {
+  res.render("foods/new.ejs")
+})
+
+//CREATE  POST users/:userId/foods (this is posting the form)
+router.post("/", async (req, res) => {
+  console.log(req.params.userId)
+  
+  try {
+    const currentUser = await User.findById(req.session.user._id)
+    console.log(currentUser)
+    
+    currentUser.pantry.push(req.body)
+
+    await currentUser.save();
+    console.log(currentUser)
+    res.redirect(`/users/${currentUser._id}/foods`)
+  } catch (error) {
+    console.log(error);
+    res.redirect("/")
+  }
+});
+
+// Show	‘/users/:userId/foods/:itemId’	GET
+// Edit	‘/users/:userId/foods/:itemId/edit’	GET
+// Update	‘/users/:userId/foods/:itemId’	PUT
+// Delete	‘/users/:userId/foods/:itemId’	DELETE
+
+
 module.exports = router;
